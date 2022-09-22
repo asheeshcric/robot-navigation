199f6d7d1feaa404c6239d57b94bab466963b12b
asheeshcric <ashiz2013@gmail.com>
2022-09-22 17:40:54 -0500

    Write a script to generate labels based on the direction the robot is moving

--- code/supervised/create_labels.py
+++ code/supervised/create_labels.py
@@ -5,47 +5,78 @@ import pickle
 import shutil
 import tqdm
 
-
-data_path = '/data/zak/rosbag/labeled'
-
-
-def get_labels(joint_filenames):
-    labels = []
-    for joint_path in joint_filenames:
-        with open(joint_path, 'rb') as joint_file:
-            vel = pickle.load(joint_file)['velocity']
-            front_avg_vel = (vel[2] + vel[3]) / 2
-            if min(vel) > 1:
-                label = 'no_obstacle'
-            elif -0.1 < front_avg_vel < 0.1:
-                label = 'obstacle'
-            else:
-                label = 'unknown'
-        labels.append(label)
-    return labels
+import pandas as pd
 
 
+data_path = '/data/zak/robot/labels'
 
-def create_csv(img_filenames, joint_filenames, dataset_path):
-    labels = get_labels(joint_filenames)
-    path_labels = list(zip(img_filenames, labels))
-    print(f'Writing labels.csv to {dataset_path}')
-    with open(os.path.join(dataset_path, 'labels.csv'), 'w') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(['img_path', 'label'])
-        writer.writerows(path_labels)
+
+# def get_labels(joint_filenames):
+#     labels = []
+#     for joint_path in joint_filenames:
+#         with open(joint_path, 'rb') as joint_file:
+#             vel = pickle.load(joint_file)['velocity']
+#             front_avg_vel = (vel[2] + vel[3]) / 2
+#             if min(vel) > 1:
+#                 label = 'no_obstacle'
+#             elif -0.1 < front_avg_vel < 0.1:
+#                 label = 'obstacle'
+#             else:
+#                 label = 'unknown'
+#         labels.append(label)
+#     return labels
+
+
+
+# def create_csv(img_filenames, joint_filenames, dataset_path):
+#     labels = get_labels(joint_filenames)
+#     path_labels = list(zip(img_filenames, labels))
+#     print(f'Writing labels.csv to {dataset_path}')
+#     with open(os.path.join(dataset_path, 'labels.csv'), 'w') as csv_file:
+#         writer = csv.writer(csv_file)
+#         writer.writerow(['img_path', 'label'])
+#         writer.writerows(path_labels)
         
-        
+
+def get_direction(joint_path):
+    with open(joint_path, 'rb') as f:
+        values = pickle.load(f)
+    velocities = values['velocity']
+    BL, BR, FL, FR = velocities
+    # Check stop condition
+    if all([-0.5 < v < 0.5 for v in velocities]):
+        return 'STOP'
+    elif all([v > 2 for v in velocities]):
+        return 'FORWARD'
+    elif all([FL > FR, BL > BR]):
+        return 'RIGHT'
+    elif all([FL < FR, BL < BR]):
+        return 'LEFT'
+    else:
+        return 'UNKNOWN'
         
 for dataset in os.listdir(data_path):
-    dataset_path = os.path.join(data_path, dataset)    
+    if '.ipynb_checkpoints' == dataset:
+        continue
+    info_csv_path = os.path.join(data_path, dataset, 'info.csv')    
 
-    # Sort all img and joints contents in ascending order and put them on a csv file with the obtained label
-    img_filenames = sorted(os.listdir(os.path.join(dataset_path, 'img')))
-    img_filenames = [os.path.join(dataset_path, 'img', img) for img in img_filenames]
-    joint_filenames = sorted(os.listdir(os.path.join(dataset_path, 'joints')))
-    joint_filenames = [os.path.join(dataset_path, 'joints', joint) for joint in joint_filenames]
-    create_csv(img_filenames, joint_filenames, dataset_path)
+    # Read info.csv file that contains img_paths, joints_paths, for all jointLabeled == True
+    info_df = pd.read_csv(info_csv_path)
+    # Get all labeled_data
+    labeled_data = info_df.loc[info_df['jointLabeled'] == True]
+    # Drop 'Unnamed: 0' Column and 'label' column
+    labeled_data = labeled_data.loc[:, ~labeled_data.columns.str.contains('^Unnamed')]
+    
+    # Get directions for labeling the data
+    labeled_data['label'] = labeled_data.joints.apply(lambda joint_path: get_direction(joint_path))
+    
+    print('---------------------------------')
+    print(f'For {dataset} Dataset:')
+    print(labeled_data.label.value_counts())
+    
+    labeled_data.to_csv(os.path.join(data_path, dataset, 'labels_directions_ashish.csv'), index=False)
+    
+    
     
     
     
